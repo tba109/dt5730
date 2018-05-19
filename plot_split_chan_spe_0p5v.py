a07a3318be48ad83d5b038309dfbe78536f64dc3
Tyler Anderson <tba109@psu.edu>
2018-05-18 22:26:25 -0400

    More work

--- plot_split_chan_spe_0p5v.py
+++ plot_split_chan_spe_0p5v.py
@@ -1,12 +1,29 @@
+#######################################################################################
+# Tyler Anderson Fri May 18 09:57:58 EDT 2018
+#
+# Plot data from the channels and do some timing analysis
+# 
+#
+#######################################################################################
+
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
 from ROOT import TH1F, TF1
 import sys
+import itertools
 
-# 0.5V range LSB to mV conversion factor
-cf = 0.030257421
+###############################################################################################
+# Program parameters
+cf = 0.030257421 # 0.5V range LSB to mV conversion factor
+NCNT = 100000
+icnt = 0
+inspect = False
+iinspect = 55
 
+
+###############################################################################################
+# Box car averager
 def box_car(x,n): 
     x1 = np.zeros(len(x)-n)
     for i in range(len(x1)): 
@@ -18,24 +35,17 @@ def box_car(x,n):
     # sys.exit()    
     return x1
 
-def cfd(x,d=1,n=4,plot=False): 
-
-    bsln = np.mean([float(xi) for xi in x[0:100]])
-    x1 = np.array([float(xi) - bsln for xi in x])
-
-    x1 = [(xi)*cf for xi in x1]
+###############################################################################################
+# Constant fraction discriminator
+def cfd(x,d=1,n=4,plot=False,ch=0,icnt=0): 
+    x1 = x
     t1 = np.arange(0,2*len(x1),2)
 
     # Box car average
     x2 = box_car(x1,n)
     t2 = np.arange(2.*n,2*(len(x1)),2)
-    # for i in range(n-1,len(x2)): 
-    #     xsum = 0
-    #     for j in range(n): 
-    #         xsum += x1[i-j]
-    #     x2[i]=float(xsum)/float(n)
-
-    num = '%03d' % icnt
+    
+    num = '%03d, %03d' % (icnt,ch)
     plt.text(200,-6,num,fontsize=20)
     plt.plot(t1,x1,'b')
     plt.plot(t2,x2,'g')
@@ -92,6 +102,35 @@ def cfd(x,d=1,n=4,plot=False):
 
     return  tz
 
+###############################################################################################
+# Trigger and Time Filter
+# x: float representing waveform samples (units = mV)
+# thr: threshold for detection (negative edge, mV)
+# d: number of samples for CFD delay
+# n: number of samples for boxcar averager
+# plot: bool representing whether the data should be plotted
+# ch: channel number
+# icnt: loop number
+# force: bool which forces analysis (good when found in one of the channels)
+# Return: True and zero time crossing
+#         False and -1000
+def ttf(x,thr=-2.1,d=1,n=4,plot=False,ch=0,icnt=0,force=False):
+    bsln = np.mean([xi for xi in x[0:100]])
+    x1 = np.array([xi - bsln for xi in x])
+    if(force): 
+        return True, cfd(x1,d,n,True,ch,icnt)
+    else:     
+        for xi in x1:
+            if(xi <= thr):
+                return True, cfd(x1,d,n,plot,ch,icnt)
+    return False, -1000
+
+###############################################################################################
+# main    
+tzero1 = np.array([])
+tzero2 = np.array([])
+iw1 = np.array([])
+iw2 = np.array([])
 
 fin1 = open('/home/tyler/DAQ/DAQ/DAQ/split_chan_spe/UNFILTERED/0@DT5730 #2-11-586_Data_split_chan_spe.csv','r')
 fin2 = open('/home/tyler/DAQ/DAQ/DAQ/split_chan_spe/UNFILTERED/1@DT5730 #2-11-586_Data_split_chan_spe.csv','r')
@@ -102,71 +141,62 @@ header2 = reader2.next()
 print header1
 print header2
 
-
-# main    
-NCNT = 20000
-icnt = 0
-tzero1 = np.array([])
-iw1 = np.array([])
-inspect = False
-iinspect = 55
-for row1 in reader1: 
-    # SPE
-    x = row1[4:]
-    bsln = np.mean([float(xi) for xi in x[0:100]])
-    x1 = np.array([float(xi) - bsln for xi in x])
-    x1 = [(xi)*cf for xi in x1]
+for row1,row2 in itertools.izip(reader1,reader2): 
+    x1 = row1[4:]
+    x1 = np.array([float(xi)*cf for xi in x1])
+    x2 = row2[4:]
+    x2 = np.array([float(xi)*cf for xi in x2])
+    # print row1[0:4],row2[0:4]
+    iw1i = int(row1[0])
+    iw2i = int(row2[0])
+    # print '%012x %012x' % (iw1i, iw2i)
     if(icnt%100==0): print 'icnt=%d' % icnt    
-    if(inspect and icnt==iinspect): cfd(x,d=4,n=8,plot=True)
-    for xi in x1:
-        if(xi <= -2.1):
-            tzero1 = np.append(tzero1,cfd(x,d=4,n=8,plot=False))
-            iw1 = np.append(iw1,icnt)
-            break
+    # plt.plot(x1); plt.plot(x2); tx = '%d' % icnt; plt.text(120,244,tx,fontsize=20); plt.show()
+    plot = False
+    if(inspect and icnt==iinspect): 
+        plot = True
+
+    # processing ch0
+    found1 = False
+    found1,tz1 = ttf(x1,thr=-2.1,d=4,n=8,plot=plot,ch=0,icnt=icnt,force=False)
+    if(found1): 
+        tzero1 = np.append(tzero1,tz1)
+        iw1 = np.append(iw1,iw1i)
+    
+    found2 = False
+    found2,tz2 = ttf(x2,thr=-2.1,d=4,n=8,plot=plot,ch=1,icnt=icnt,force=False)
+    if(found2): 
+        tzero2 = np.append(tzero2,tz2)
+        iw2 = np.append(iw2,iw2i)
+
     icnt+=1
     if(icnt == NCNT): 
         break
     
-icnt = 0
-tzero2 = np.array([])
-iw2 = np.array([])
-for row2 in reader2: 
-    # SPE
-    x = row2[4:]
-    bsln = np.mean([float(xi) for xi in x[0:100]])
-    x1 = np.array([float(xi) - bsln for xi in x])
-    x1 = [(xi)*cf for xi in x1]
-    if(icnt%100==0): print 'icnt=%d' % icnt
-    if(inspect and icnt==iinspect): cfd(x,d=4,n=8,plot=True)
-    for xi in x1:
-        if(xi <= -2.1):
-            tzero2 = np.append(tzero2,cfd(x,d=4,n=8,plot=False))
-            iw2 = np.append(iw2,icnt)
-            break
-    icnt+=1
-    if(icnt == NCNT): 
-        break
+print len(tzero1), len(tzero2), len(iw1), len(iw2)
 
-print len(tzero1), len(tzero2), len(iw1)
+td = [tz1i - tz2i for tz1i,tz2i in zip(tzero1,tzero2)]
 
-fout = open("fout.txt",'w')    
-td = [tz1 - tz2 for tz1,tz2 in zip(tzero1,tzero2)]
-plt.plot(td)
-plt.show()
-print np.std(td)
-# print td
 h1 = TH1F("h1","",1000,-50,50)
 i = 0
-for tdi,iwi in zip(td,iw1): 
+for tdi,iw1i,iw2i in zip(td,iw1,iw2): 
     h1.Fill(tdi)
-    if(tdi > 0.5 or tdi < -0.5):
-        strout = "%d %f\n" % (iwi,tdi)
-        fout.write(strout)
     i+=1
 
-fout.close()
+h2 = TH1F("h2","",1000,-50,50)
+# h2.SetLineColor(2)
+h2.GetXaxis().CenterTitle()
+i = 0
+for tdi,iw1i,iw2i in zip(td,iw1,iw2): 
+    if((int(iw1i) & 0xffff0000) == (int(iw2i) & 0xffff0000)):
+        h2.Fill(tdi)
+        i+=1
 
-h1.GetXaxis().SetTitle("CFD CH0-CH1 #deltaT (ns)")
-h1.Draw()
+h2.GetXaxis().SetTitle("CFD CH0-CH1 #deltaT (ns)")
+h2.GetXaxis().CenterTitle()
+# h1.Draw()
+# h2.Draw("same")
+
+h2.Draw("")
 f1 = TF1("f1","gaus")
-h1.Fit(f1)
+h2.Fit(f1)
